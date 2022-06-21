ebc21afad3e1c4f5cbe54cd9580fcea85f622cc7
Alice Medvedeva <alisa.medvedeva@optimax.dev>
2022-06-22 01:27:49 +0500

    Реактивность на прокси у vue 3

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, idx) in items" :key="idx">
+        {{ item.name }} - {{ item.price }}
+      </li>
+    </ul>
+    <button @click="addAndUpdate">Add and update</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      items: [
+        { name: "Alph", price: 1 },
+        { name: "Beta", price: 2 },
+      ],
+    };
+  },
+
+  methods: {
+    addAndUpdate() {
+      const newItem = { name: "New", price: "-" };
+      this.items.push(newItem);
+      const createdElm = this.items[this.items.length - 1];
+      setTimeout(() => {
+        // выведет Proxy
+        console.log(this.items);
+        createdElm.price = "over 9000";
+        // подход vue 2 не сработает объект возвращаемой функцией data -
+        // это не объект с геттерами/сеттерами, это объект обернутый в Proxy
+        // newItem.price = "over 9000";
+      }, 3000);
+    },
+  },
+};
+</script>
